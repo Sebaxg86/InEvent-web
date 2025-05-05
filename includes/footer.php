14140544f4177677df860a17b806113a6269e710
Sebastian Chairez <sebax86@icloud.com>
2025-05-05 13:02:35 -0600

    includes/footer.php ADDED

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+<footer>
+        <div class="footer-title-div">
+            <h4>InEvent</h4>
+            <p>Your gateway to unforgettable experiences.</p>
+        </div>
+        
+        <nav class="footer-nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="events.php">Events</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="register.php">Register</a></li>
+            </ul>
+        </nav>
+
+        <div class="socials-div">
+            <p>
+                Follow us:
+                <a href="#">Facebook <ion-icon name="logo-facebook"></ion-icon> |</a> 
+                <a href="#">Twitter <ion-icon name="logo-twitter"></ion-icon> |</a> 
+                <a href="#">Instagram <ion-icon name="logo-instagram"></ion-icon></a>
+            </p>
+            <p>&copy; 2025 InEvent. All rights reserved. By: Sebastian Chairez</p>
+        </div>
+    </footer>
