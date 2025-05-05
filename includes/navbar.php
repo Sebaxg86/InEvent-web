5c9ee1df44b08b5998a85598ee573af250ae7afb
Sebastian Chairez <sebax86@icloud.com>
2025-05-05 12:58:52 -0600

    includes/navbar.php ADDED

---
+++ includes/navbar.php
@@ -0,0 +1,9 @@
+<nav class="nav-bar">
+    <ul>
+        <li><a href="index.html">Home <ion-icon name="home-outline"></ion-icon></a></li>
+        <li><a href="events.html">Events <ion-icon name="ticket-outline"></ion-icon></a></li>
+        <li><a href="about.html">About us <ion-icon name="information-circle-outline"></ion-icon></a></li>
+        <li><a href="contact.html">Contact <ion-icon name="paper-plane-outline"></ion-icon></a></li>
+        <li><a href="register.html">Register <ion-icon name="log-in-outline"></ion-icon></a></li>
+    </ul>
+</nav>
