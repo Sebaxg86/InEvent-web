fe335c37498bdcca5bb47c294198540a0fd81d6a
Sebastian Chairez <sebax86@icloud.com>
2025-05-05 16:14:39 -0600

    includes/register_account.php ADDED

---
+++ includes/register_account.php
@@ -0,0 +1,23 @@
+<section class="welcome-message">
+    <h2>Create an Account</h2>
+</section>
+
+<div class="content-container">
+    <div class="content-form">
+        <form action="#" method="post">
+        <label for="fullname">Full Name:</label><br>
+                <input type="text" id="fullname" name="fullname" required><br><br>
+    
+                <label for="email">Email Address:</label><br>
+                <input type="email" id="email" name="email" required><br><br>
+    
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password" required><br><br>
+    
+                <label for="confirm-password">Confirm Password:</label><br>
+                <input type="password" id="confirm-password" name="confirm-password" required><br><br>
+    
+                <button class="btn" type="submit">Register</button>
+        </form>
+    </div>
+</div>
