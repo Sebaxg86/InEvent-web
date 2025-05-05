6c5c0967fccac3ff6cf48d6189647b98996d6b56
Sebastian Chairez <sebax86@icloud.com>
2025-05-05 13:03:52 -0600

    includes/navbar.php UPDATED

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,9 +1,9 @@
 <nav class="nav-bar">
     <ul>
-        <li><a href="index.html">Home <ion-icon name="home-outline"></ion-icon></a></li>
-        <li><a href="events.html">Events <ion-icon name="ticket-outline"></ion-icon></a></li>
-        <li><a href="about.html">About us <ion-icon name="information-circle-outline"></ion-icon></a></li>
-        <li><a href="contact.html">Contact <ion-icon name="paper-plane-outline"></ion-icon></a></li>
-        <li><a href="register.html">Register <ion-icon name="log-in-outline"></ion-icon></a></li>
+        <li><a href="index.php">Home <ion-icon name="home-outline"></ion-icon></a></li>
+        <li><a href="events.php">Events <ion-icon name="ticket-outline"></ion-icon></a></li>
+        <li><a href="about.php">About us <ion-icon name="information-circle-outline"></ion-icon></a></li>
+        <li><a href="contact.php">Contact <ion-icon name="paper-plane-outline"></ion-icon></a></li>
+        <li><a href="register.php">Register <ion-icon name="log-in-outline"></ion-icon></a></li>
     </ul>
 </nav>
