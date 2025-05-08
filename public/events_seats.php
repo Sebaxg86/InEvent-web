7d6c10f5baa19c03b2a89b6c61a480b289b637f2
Sebastian Chairez <sebax86@icloud.com>
2025-05-08 00:18:45 -0600

    events_seats.php ADDEED

---
+++ public/events_seats.php
@@ -0,0 +1,40 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!--Styles-->
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/events.css">
+
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+    
+    <title>InEvent</title>
+</head>
+<body>
+    <!--Header-->
+    <?php include_once '../includes/header.php'; ?>
+
+    <!--Navbar-->
+    <?php include_once '../includes/navbar.php'; ?>
+
+    <main>
+        
+    </main>
+
+    <!--Footer-->
+    <?php include_once "../includes/footer.php"; ?>
+
+    <!--Ionic Icons Installation-->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
