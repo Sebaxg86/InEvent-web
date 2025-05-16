f053b0f9da6fe3947d6beccf67389221e741b7a6
Sebastian Chairez <sebax86@icloud.com>
2025-05-16 01:45:55 -0600

    events list uPDATED

--- includes/events_list_public.php
+++ includes/events_list_public.php
@@ -1,4 +1,4 @@
-<section class="welcome-message">
+<section class="browse-message">
     <h2>Browse Events</h2>
 </section>
 
@@ -20,15 +20,19 @@
                 ]);
                 $btnText = $usesSeats ? 'View Seats' : 'Buy Tickets';
             ?>
+
             <div class="event-card">
-                <img src="<?php echo $event['img']; ?>" alt="<?php echo $event['title']; ?>" style="width:100%; height:auto;">
-                <h3><?php echo $event['title']; ?></h3>
-                <p>Date: <?php echo date('F j, Y · g:i A', strtotime($event['event_date'])); ?></p>
-                <p>Location: <?php echo $event['venue']; ?></p>
-                <p>Type: <?php echo $event['type']; ?></p>
-                <p>From $<?php echo number_format($event['price'], 2); ?> MXN</p>
-                <a href="events_seats.php?id=<?php echo $event['id']; ?>" class="btn btn-primary"><?php echo $btnText; ?></a>
+                <img src="<?php echo $event['img']; ?>" alt="<?php echo $event['title']; ?>">
+                <div class="event-info">
+                    <h3><?php echo $event['title']; ?></h3>
+                    <p>Date: <?php echo date('F j, Y · g:i A', strtotime($event['event_date'])); ?></p>
+                    <p>Location: <?php echo $event['venue']; ?></p>
+                    <p>Type: <?php echo $event['type']; ?></p>
+                    <p>From $<?php echo number_format($event['price'], 2); ?> MXN</p>
+                    <a href="events_seats.php?id=<?php echo $event['id']; ?>" class="btn"><?php echo $btnText; ?></a>
+                </div>
             </div>
+
         <?php endwhile; ?>
     </div>
 </section>
